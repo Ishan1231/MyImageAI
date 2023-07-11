7163eaf9833bbdaa4eab97ed040170e395ee234f
Ishan1231 <ishanmishra1231@gmail.com>
2023-07-11 15:55:18 +0530

    I CHANGED SMTH

--- main.py
+++ main.py
@@ -12,3 +12,5 @@ prediction.loadModel()
 predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "house.jpg"), result_count=5)
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction, " : ", eachProbability)
+
+#I changed smthingg
